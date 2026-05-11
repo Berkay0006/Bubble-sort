6eac40b0e233c2025686db07871972cfc5c59489
Berkay0006 <s256458@dtu.dk>
2026-05-11 12:41:25 +0200

    initial

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+    void swap(char *a, char *b){
+        char t = *a; 
+        *a = *b; 
+        *b = t;
+    }
+
+    bool letters_in_order(char a, char b){
+        return tolower(a) <= tolower(b);
+    }
+
+    bool numbers_in_ascending_order(char a, char b){
+        return a <= b;
+    }
+
+    void bubble_sort(char a[], int n, bool (*ok)(char, char)){
+ 
+    bool swapped; 
+  
+    do{swapped = 0; 
+    
+    for(int i = 1; i<n; i++) 
+    if(!ok(a[i-1], a[i])){      
+    swap(&a[i-1], &a[i]);           
+    swapped = 1;                           
+    }}
+
+    while(swapped);                         
+    }
+
+int main (void) {
+    char letter_arr1[] = {'z', 'S', 's', 'a'}; 
+    char number_arr1[] = {4, -1, 2, 9};
+    bubble_sort(letter_arr1, 4, letters_in_order);
+    bubble_sort(number_arr1, 4, numbers_in_ascending_order);
+   
+    for(int i = 0; i < 4; i++)
+    printf("%c", letter_arr1[i]); puts("");
+   
+    for(int i = 0; i < 4; i++)
+
+    printf("%c", letter_arr1[i]); puts("");
+
+    for(int i = 0; i < 4; i++)
+
+    printf("%d ", number_arr1[i]); puts("");
+
+return 0; 
+}
